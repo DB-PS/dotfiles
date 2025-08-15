e9d6b749dd1c75cd017d8e586ac1e1de5311bda6
DB-PS <darshanbagrecha1729@gmail.com>
2025-08-15 14:52:45 +0530

    Fixed README.md and init.sh

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Run to install everything:
 
 ```bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/DarshanBagrecha-TI/dotfiles/refs/heads/main/.init.sh?token=)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/DB-PS/dotfiles/main/init.sh)"
 ```
--- init.sh
+++ init.sh
@@ -9,13 +9,14 @@ xcode-select --install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#git clone --recursive git@github.com:DarshanBagrecha-TI/dotfiles.git
+git clone --recursive git@github.com:DB-PS/dotfiles.git
 
 omz reload
-
-args brew install < .brew
 
 sudo ln -sfn realpath .java/zulu-8.jdk /Library/Java/JavaVirtualMachines
 sudo ln -sfn realpath .java/jdk-24.jdk /Library/Java/JavaVirtualMachines
 
 cd ~/dotfiles && stow -S . --ignore=".java" && ~
+
+cd ~
+xargs brew install < .brew
